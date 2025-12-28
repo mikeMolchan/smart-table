1f6f1f1bf29f845cca73e8e24d4cba6f18640f4a
Misha Molchan <mikke.molchan@gmail.com>
2025-12-28 13:37:46 +0300

    First version of project

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,14 +1,35 @@
 import {createComparison, defaultRules} from "../lib/compare.js";
 
-// @todo: #4.3 — настроить компаратор
-
 export function initFiltering(elements, indexes) {
-    // @todo: #4.1 — заполнить выпадающие списки опциями
+    // Заполняем выпадающие списки опциями
+    Object.keys(indexes)                                    // Получаем ключи из объекта
+      .forEach((elementName) => {                        // Перебираем по именам
+        elements[elementName].append(                    // в каждый элемент добавляем опции
+            ...Object.values(indexes[elementName])        // формируем массив имён, значений опций
+                      .map(name => {                        // используйте name как значение и текстовое содержимое
+                          const option = document.createElement('option');
+                          option.value = name;
+                          option.textContent = name;
+                          return option;
+                      })
+        )
+     })
+
+    // Настраиваем компаратор
+    const compare = createComparison(defaultRules);
 
     return (data, state, action) => {
-        // @todo: #4.2 — обработать очистку поля
+        const filterState = {...state};
+        if (filterState.totalFrom || filterState.totalTo) {
+            filterState.total = [
+                filterState.totalFrom ? parseFloat(filterState.totalFrom) : '',
+                filterState.totalTo ? parseFloat(filterState.totalTo) : ''
+            ];
+        }
+        delete filterState.totalFrom;
+        delete filterState.totalTo;
 
-        // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data;
+        // Отфильтровываем данные используя компаратор
+        return data.filter(row => compare(row, filterState));
     }
 }
